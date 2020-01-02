642014615cdae118d9eb9486d4740a446d6977be
Yizhi <lycheesun@hotmail.com>
2020-01-01 22:39:15 -0500

    minor updates

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static('static'));
+//app.use(express.static('static'));
 
 // app.use(express.json({limit: '1mb'}));
 
-// app.use('/public', express.static(path.join(__dirname,'static')));
-// app.get('/', (req,res)=>{
-//     res.sendFile(path.join(__dirname,'static', 'index.html'));
-// });
+app.use('/public', express.static(path.join(__dirname,'static')));
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'static', 'index.html'));
+});
 app.get('/:page', (req,res)=>{
     res.sendFile(path.join(__dirname,'static', req.params.page));
 });
