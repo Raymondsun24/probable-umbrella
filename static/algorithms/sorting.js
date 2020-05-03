9b18c64e2b9eb90d95a6fe2b0df2a627cb2506cf
Raymond <lycheesun@hotmail.com>
2020-05-02 23:19:37 -0400

    newchanges

--- static/algorithms/sorting.js
+++ static/algorithms/sorting.js
@@ -1,13 +1,14 @@
 const WIDTH = 800
 const HEIGHT = 800
+const k = 10
 
 function setup(){
     createCanvas(WIDTH, HEIGHT);
 }
 
 function getData(){
-    var data = new Array(10000);
-    for(var i = 0; i < 10000; i++){
+    var data = new Array(k);
+    for(var i = 0; i < k; i++){
         data[i] = Math.random()*100;
     }
     return data;
@@ -17,15 +18,47 @@ var data = getData();
 console.log(data);
 
 
+function swap(index1, index2){
+    if(index1 == index2) return;
+    var temp = data[index2];
+    console.log(data[index1], data[index2]);
+    data[index2] = data[index1];
+    data[index1] = temp;
+    console.log(data[index1], data[index2]);
+    console.log(data);
+}
+
+async function selectionSort(){
+    for(var i = 0; i < k; i++){
+        var a = await setTimeout(ss, 200, i);
+        console.log(i);
+    }
+}
+
+function ss(i){
+    console.log(i);
+    var minIndex = i;
+    var min = data[i];
+    for(var j = i; j < k; j++){
+        if(data[j] < data[i]){
+            minIndex = j;
+            min = data[j];
+        }
+    }
+    swap(i, minIndex);
+}
 
 function draw(){
     line(0, 0, 0, HEIGHT);
     line(0, 0, WIDTH, 0);
     line(0, HEIGHT, WIDTH, HEIGHT);
     line(WIDTH, 0, WIDTH, HEIGHT);
-    const W = WIDTH/10000;
-    for(var i = 0; i < 10000; i++){
+    const W = WIDTH/k;
+    for(var i = 0; i < k; i++){
         rect(i*W, 800-(data[i]*8), W, (data[i]*8));
     }
 }
 
+var a = selectionSort();
+console.log('a');
+console.log(data);
